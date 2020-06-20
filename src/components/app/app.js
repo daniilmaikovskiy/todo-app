5623162ab99e8933e25f58476a4a0cc30192da7f
danil maikovskiy <maikovsky.danil@gmail.com>
2020-06-20 21:37:20 +0300

    main tag

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './app.css';
 import Header from '../header';
+import Main from '../main';
 
 export default function App() {
     return (
       <section className='todoapp'>
         <Header />
+        <Main>
+          
+        </Main>
       </section>
     );
   };
---
+++ src/components/main/main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './main.css';
+
+export default function Main() {
+    return <section className='main' />;
+}
