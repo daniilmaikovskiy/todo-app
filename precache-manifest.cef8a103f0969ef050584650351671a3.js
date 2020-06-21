408e1782a207a97f6112ac2ee9351d7c500ffc73
danil maikovskiy <maikovsky.danil@gmail.com>
2020-06-21 13:32:28 +0300

    Updates

---
+++ precache-manifest.cef8a103f0969ef050584650351671a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d96079e72712c9fe01f19bb015c571",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "f773eac48598a8a69e70",
+    "url": "/todo-app/static/css/main.a6663e7f.chunk.css"
+  },
+  {
+    "revision": "3f1caefc9a11d5bc1c8f",
+    "url": "/todo-app/static/js/2.da5d696f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.da5d696f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f773eac48598a8a69e70",
+    "url": "/todo-app/static/js/main.d8e88330.chunk.js"
+  },
+  {
+    "revision": "59a60f66a7179493024f",
+    "url": "/todo-app/static/js/runtime-main.f617a8c6.js"
+  }
+]);
