d4f2a026dd9b530cf9ded6f18e5271d3098444a4
danil maikovskiy <maikovsky.danil@gmail.com>
2020-06-21 12:02:21 +0300

    footer added

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './main.css';
 import TodoList from '../todo-list';
+import Footer from '../footer';
 
 export default function Main({ tasks }) {
     return (
         <section className='main'>
             <TodoList tasks={ tasks } />
+            <Footer />
         </section>
     );
 }
