a1e49716cdd5402647e7288576464082655a4fbf
danil maikovskiy <maikovsky.danil@gmail.com>
2020-06-21 13:49:05 +0300

    refactoring

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -4,23 +4,23 @@ import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 
 export default function Task({ className, description, created }) {
 
-    let editInput = className === 'editing' 
-      ? <input type="text" className="edit" defaultValue={ description } /> : null;
+  let editInput = className === 'editing' 
+    ? <input type="text" className="edit" defaultValue={ description } /> : null;
     
-    return (
-        <li className={ className }>
-            <div className="view">
-              <input className="toggle" type="checkbox" />
-              <label>
-                <span className="description">{ description }</span>
-                <span className="created">
-                  { formatDistanceToNow(created, { addSuffix: true }) }
-                </span>
-              </label>
-              <button className="icon icon-edit"></button>
-              <button className="icon icon-destroy"></button>
-            </div>
-            { editInput }
-        </li>
-    );
+  return (
+    <li className={ className }>
+      <div className="view">
+        <input className="toggle" type="checkbox" />
+        <label>
+          <span className="description">{ description }</span>
+          <span className="created">
+            { formatDistanceToNow(created, { addSuffix: true }) }
+          </span>
+        </label>
+        <button className="icon icon-edit"></button>
+        <button className="icon icon-destroy"></button>
+      </div>
+      { editInput }
+    </li>
+  );
 }
