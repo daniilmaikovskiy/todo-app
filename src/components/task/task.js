a012581bff5363c5ebcb95abaf26d9050d2bceaf
danil maikovskiy <maikovsky.danil@gmail.com>
2020-06-25 18:13:59 +0300

    Task refactoring

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -1,44 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './task.css';
 import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 
-export default function Task(
-  { onDeleted, onCompleted, onEdited, onClickEditButton, 
-    className, description, created, filter }) {
-  
-  let isCompleted = className.indexOf('completed') + 1;
-  let editInput = null;
+export default class Task extends Component {
+  static propTypes = {
+    onDeleted:         PropTypes.func.isRequired,
+    onCompleted:       PropTypes.func.isRequired,
+    onClickEditButton: PropTypes.func.isRequired,
 
-  if (className.indexOf('editing') + 1) {
-    let secondClass = 'edit-' + (isCompleted ? 'completed' : 'active');
-    let editClassName = 'edit ' + secondClass;
+    className:   PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    created:     PropTypes.objectOf(Date).isRequired,
 
-    editInput = (
-      <form onSubmit={ e => { e.preventDefault(); onEdited(e.target.input.value) } }>
-        <input type="text" className={ editClassName } 
-          onChange={ e => e.target.value = e.target.value.trimLeft() }
-          defaultValue={ description } name='input' />
-      </form>
-    );
+    filter: PropTypes.string.isRequired,
   }
 
-  let isHidden = ((filter === 'completed' && !isCompleted) ||
-                  (filter === 'active' && isCompleted));
-    
-  return (
-    <li className={ className + (isHidden ? ' hidden' : '') }>
-      <div className="view">
-        <input className="toggle" type="checkbox" />
-        <label onClick={ onCompleted }>
-          <span className="description">{ description }</span>
-          <span className="created">
-            { formatDistanceToNow(created, { addSuffix: true }) }
-          </span>
-        </label>
-        <button className="icon icon-edit" onClick={ onClickEditButton }></button>
-        <button className="icon icon-destroy" onClick={ onDeleted }></button>
-      </div>
-      { editInput }
-    </li>
-  );
+  getEditInput = (className, isCompleted) => {
+    if (className.indexOf('editing') + 1) {
+      let { onEdited, description } = this.props;
+
+      let secondClass = 'edit-' + (isCompleted ? 'completed' : 'active');
+      let editClassName = 'edit ' + secondClass;
+
+      return (
+        <form onSubmit={ e => { e.preventDefault(); onEdited(e.target.input.value) } }>
+          <input type="text" className={ editClassName } 
+            onChange={ e => e.target.value = e.target.value.trimLeft() }
+            defaultValue={ description } name='input' />
+        </form>
+      );
+    }
+
+    return null;
+  }
+
+  render() {
+    let { onDeleted, onCompleted, onClickEditButton, 
+      className, description, created, filter } = this.props;
+    let { getEditInput } = this;
+
+    let isCompleted = className.indexOf('completed') + 1;
+    let isHidden = ((filter === 'completed' && !isCompleted) ||
+                    (filter === 'active' && isCompleted));
+      
+    return (
+      <li className={ className + (isHidden ? ' hidden' : '') }>
+        <div className="view">
+          <input className="toggle" type="checkbox" />
+          <label onClick={ onCompleted }>
+            <span className="description">{ description }</span>
+            <span className="created">
+              { formatDistanceToNow(created, { addSuffix: true }) }
+            </span>
+          </label>
+          <button className="icon icon-edit" onClick={ onClickEditButton }></button>
+          <button className="icon icon-destroy" onClick={ onDeleted }></button>
+        </div>
+        { getEditInput(className, isCompleted) }
+      </li>
+    );
+  }
 }
