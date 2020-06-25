c702ca09d223c2072e25118f9d517857e34bcebe
danil maikovskiy <maikovsky.danil@gmail.com>
2020-06-25 18:20:28 +0300

    Footer refactoring

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './footer.css';
 import TasksFilter from '../tasks-filter';
 
-export default function Footer({ setFilter, filter, clearCompleted, completedCount }) {
-    return (
-        <footer className="footer">
-            <span className="todo-count">{ completedCount } items left</span>
-            <TasksFilter setFilter={ setFilter } filter={ filter } />
-            <button className="clear-completed" onClick={ clearCompleted } >
-                Clear completed
-            </button>
-        </footer>
-    );
+export default class Footer extends Component {
+    static propTypes = {
+        setFilter:      PropTypes.func.isRequired, 
+        clearCompleted: PropTypes.func.isRequired, 
+        filter:         PropTypes.string.isRequired, 
+        completedCount: PropTypes.number.isRequired,
+    }
+
+    render() {
+        let { setFilter, filter, clearCompleted, completedCount } = this.props;
+
+        return (
+            <footer className="footer">
+                <span className="todo-count">{ completedCount } items left</span>
+                <TasksFilter setFilter={ setFilter } filter={ filter } />
+                <button className="clear-completed" onClick={ clearCompleted } >
+                    Clear completed
+                </button>
+            </footer>
+        );
+    }
 }
