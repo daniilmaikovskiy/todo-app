97699915bf8e704e7a5ac817e1ccfa3bbd065f08
danil maikovskiy <maikovsky.danil@gmail.com>
2020-06-25 17:56:11 +0300

    TodoList refactoring

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './todo-list.css';
 import Task from '../task';
 
-export default function TodoList(
-    { tasks, filter, onDeleted, onEdited, onCompleted, onClickEditButton }) {
+export default class TodoList extends Component {
+    static propTypes = {
+        tasks: PropTypes.arrayOf(PropTypes.shape({
+            id:          PropTypes.number.isRequired,
+            className:   PropTypes.string.isRequired,
+            description: PropTypes.string.isRequired,
+            created:     PropTypes.objectOf(Date).isRequired,
+        })).isRequired,
 
-    const taskArr = tasks.map(taskData => {
-        let { id, ...taskProps } = taskData;
-    
-        return <Task key={ id } { ...taskProps } 
-            filter={ filter }
-            onDeleted={ () => onDeleted(id) } 
-            onEdited={ text => onEdited(id, text) } 
-            onCompleted={ () => { onCompleted(id) } }
-            onClickEditButton={ () => { onClickEditButton(id) } } />
-    });
-    
-    return (
-        <ul className="todo-list">
-            { taskArr }
-        </ul>
-    );
+        filter:            PropTypes.string.isRequired,
+        onDeleted:         PropTypes.func.isRequired,
+        onEdited:          PropTypes.func.isRequired,
+        onCompleted:       PropTypes.func.isRequired,
+        onClickEditButton: PropTypes.func.isRequired,
+    }
+
+    render() {
+        let { tasks, filter, onDeleted, onEdited, onCompleted, 
+            onClickEditButton } = this.props;
+
+        const taskArr = tasks.map(taskData => {
+            let { id, ...taskProps } = taskData;
+            
+            return <Task key={ id } { ...taskProps } 
+                filter={ filter }
+                onDeleted={ () => onDeleted(id) } 
+                onEdited={ text => onEdited(id, text) } 
+                onCompleted={ () => { onCompleted(id) } }
+                onClickEditButton={ () => { onClickEditButton(id) } } />
+        });
+            
+        return <ul className="todo-list">{ taskArr }</ul>
+    }
 }
