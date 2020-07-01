b0ebb9d563eed2d9003d6b4ec58166e65c10c41a
danil maikovskiy <maikovsky.danil@gmail.com>
2020-07-01 16:24:06 +0300

    refactoring

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,25 +3,21 @@ import PropTypes from 'prop-types';
 import './footer.css';
 import TasksFilter from '../tasks-filter';
 
-export default class Footer extends React.PureComponent {
-  static propTypes = {
-    setFilter: PropTypes.func.isRequired,
-    clearCompleted: PropTypes.func.isRequired,
-    filter: PropTypes.string.isRequired,
-    completedCount: PropTypes.number.isRequired,
-  };
-
-  render() {
-    const { setFilter, filter, clearCompleted, completedCount } = this.props;
-
-    return (
-      <footer className="footer">
-        <span className="todo-count">{completedCount} items left</span>
-        <TasksFilter setFilter={setFilter} filter={filter} />
-        <button type="button" className="clear-completed" onClick={clearCompleted}>
-          Clear completed
-        </button>
-      </footer>
-    );
-  }
+export default function Footer({ setFilter, filter, clearCompleted, completedCount }) {
+  return (
+    <footer className="footer">
+      <span className="todo-count">{completedCount} items left</span>
+      <TasksFilter setFilter={setFilter} filter={filter} />
+      <button type="button" className="clear-completed" onClick={clearCompleted}>
+        Clear completed
+      </button>
+    </footer>
+  );
 }
+
+Footer.propTypes = {
+  setFilter: PropTypes.func.isRequired,
+  clearCompleted: PropTypes.func.isRequired,
+  filter: PropTypes.string.isRequired,
+  completedCount: PropTypes.number.isRequired,
+};
