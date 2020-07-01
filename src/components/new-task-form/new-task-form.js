38752ea39d8280c1df3e2ff1eda722573c84e710
danil maikovskiy <maikovsky.danil@gmail.com>
2020-07-01 17:14:46 +0300

    refactoring

--- src/components/new-task-form/new-task-form.js
+++ src/components/new-task-form/new-task-form.js
@@ -2,40 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './new-task-form.css';
 
-export default class NewTaskForm extends React.Component {
-  static propTypes = {
-    value: PropTypes.string.isRequired,
-    addNewTask: PropTypes.func.isRequired,
-    onChange: PropTypes.func.isRequired,
-  };
-
-  onSubmit = (e) => {
-    const { addNewTask } = this.props;
-
+export default function NewTaskForm({ addNewTask, onChange, value }) {
+  const onSubmit = (e) => {
     e.preventDefault();
     addNewTask(e.target.input.value);
   };
 
-  onChange = (e) => {
-    const { onChange } = this.props;
-
-    onChange(e.target.value);
-  };
-
-  render() {
-    const { value } = this.props;
-    const { onSubmit, onChange } = this;
-
-    return (
-      <form onSubmit={onSubmit}>
-        <input
-          className="new-todo"
-          name="input"
-          value={value}
-          onChange={onChange}
-          placeholder="What needs to be done?"
-        />
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        className="new-todo"
+        name="input"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder="What needs to be done?"
+      />
+    </form>
+  );
 }
+
+NewTaskForm.propTypes = {
+  value: PropTypes.string.isRequired,
+  addNewTask: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
